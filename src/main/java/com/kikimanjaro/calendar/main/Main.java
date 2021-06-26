bd1641736d5be6fbc748589d51bf953dfaadc3a9
KikiManjaro <kylian.meulin@sas-impact.fr>
2021-06-26 21:24:02 +0200

    Fix logger

--- src/main/java/com/kikimanjaro/calendar/main/Main.java
+++ src/main/java/com/kikimanjaro/calendar/main/Main.java
@@ -1,11 +1,18 @@
 package com.kikimanjaro.calendar.main;
 
 import com.kikimanjaro.calendar.main.ui.MainFrame;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.TimeZone;
 
 public class Main {
+    protected static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        BasicConfigurator.configure();
+        log.info("Running application");
         TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
         new MainFrame();
     }
